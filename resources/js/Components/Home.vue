9c5bca31f6e42a0dc9553f28984a4c7297e81e7e
marekkrzysztofik <marekkrzysztofik2@wp.pl>
2023-06-15 18:04:08 +0200

    total expenses

--- resources/js/Components/Home.vue
+++ resources/js/Components/Home.vue
@@ -1,9 +1,19 @@
 <template>
     <h1 class="m-3">Strona główna</h1>
     <div class="grid-box">
+        <div class="flex">
+            <div class="p-20"><h1>Expense</h1></div>
+            <div class="p-20">
+                <h1>{{ totalExpenses }}</h1>
+            </div>
+        </div>
         <div v-for="item in expenses" class="flex">
             <div class="p-20">{{ item.name }}</div>
-            <div class="p-20">{{ item.price }}</div>
+            <div class="p-20">
+                {{ item.day }}. {{ item.month }}.
+                {{ item.year }}
+            </div>
+            <div class="p-20">{{ item.price }} zł</div>
         </div>
     </div>
 </template>
@@ -11,23 +21,38 @@
 import axios from "axios";
 import { onMounted, reactive, ref, Ref } from "vue";
 import { useRouter } from "vue-router";
-import { useCategories } from "@/../utils/useCompetitors";
-const router = useRouter();
 
-const { getCategories, categories } = useCategories();
+interface Expense {
+    name: string;
+    day: number;
+    month: number;
+    year: number;
+    price: number;
+    time: string;
+}
+const router = useRouter();
+const expensesArray: Array<number> = [];
+const expenses: Ref<Array<Expense>> = ref([]);
+const totalExpenses: Ref = ref();
 
 onMounted(async () => {
     const currentDate = new Date();
     const currentMonth: number = currentDate.getMonth() + 2;
-    getExpensesByMonth(currentMonth);
-    getCategories();
+    await getExpensesByMonth(currentMonth);
 });
-const expenses = ref([]);
+
 const getExpensesByMonth = async (currentMonth: number) => {
     const response = await axios.get(
         `/api/getExpensesJoinedWithCategoriesByMonth/${currentMonth}`
     );
     expenses.value = response.data;
+    expenseCounter();
+};
+const expenseCounter = () => {
+    expenses.value.forEach((el) => {
+        expensesArray.push(el.price);
+    });
+    totalExpenses.value = expensesArray.reduce((a, b) => a + b);
 };
 </script>
 <style scoped>
