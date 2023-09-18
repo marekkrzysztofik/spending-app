e2897fe893f74c16d2eb754afea4bc1ee09c23da
marekkrzysztofik <marekkrzysztofik2@wp.pl>
2023-09-18 16:55:45 +0200

    add transaction

--- resources/js/Components/App.vue
+++ resources/js/Components/App.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="flex">
-        <nav>
-            <div class="flex flex-column header__nav">
-                <router-link to="/">
-                    <h1 class="primary-heading mb-3">Spending tracker</h1>
-                </router-link>
-                <nav class="flex flex-column">
-                    <div class="sidemenu-item" v-for="(menuItem, index) in menuItems" :key="index">
-                        <router-link class="sidemenu-link" :to="menuItem.path">
-                            <i :class="menuItem.icon" /> {{ menuItem.label }}
-                        </router-link>
-                    </div>
-                </nav>
+        <nav class="h-screen m-4">
+            <router-link to="/">
+                <h1 class="primary-heading mb-3">Spending tracker</h1>
+            </router-link>
+                <div class="sidemenu-item" v-for="(menuItem, index) in menuItems" :key="index">
+                    <router-link class="sidemenu-link" :to="menuItem.path">
+                        <i :class="menuItem.icon" /> {{ menuItem.label }}
+                    </router-link>
+                </div>
+            <div class="new-transaction">
+                <Button class="add-button" icon="pi pi-plus" unstyled/>
+               <p class="my-3">Add transaction</p>
             </div>
         </nav>
         <router-view />
@@ -40,6 +40,28 @@ a {
     color: black;
     text-decoration: none;
 }
+.new-transaction {
+    margin-top: 1rem;
+    display: flex;
+    border-style: dashed;
+    padding: 1.5rem 0;
+    border-width: 2px;
+    border-color: rgba(0, 119, 6, 0.3);
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+}
+.add-button {
+    border: none;
+    border-radius: 50%;
+    height: 3rem;
+    width: 3rem;
+    background-color: rgba(0, 119, 6, 0.7);
+
+}
+.nav-bar:last-child {
+    align-items: flex-end;
+}
 
 .sidemenu-link {
     color: rgb(153, 153, 153);
@@ -60,9 +82,5 @@ a {
     padding: 0.65rem 1rem;
     border-radius: 10px;
     transition: color 0.2s ease;
-}
-
-.header__nav {
-    margin: 2rem;
 }
 </style>
