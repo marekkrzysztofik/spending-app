de4a1e17fdf81612d7d1d29307d2d64a87145ec5
marekkrzysztofik <marekkrzysztofik2@wp.pl>
2024-01-22 17:19:39 +0100

    delete budget

--- resources/js/Modules/Categories/Categories.vue
+++ resources/js/Modules/Categories/Categories.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex m-3">
-    <ScrollPanel style="height: 520px">
+    <ScrollPanel style="height: 520px; width:17%">
       <div class="flex flex-column">
-        <div @click="get(index)" v-for="(budget, index) in budgetsWithClasses" class="flex m-1 sidemenu-item"
-          :class=budget.class>
+        <div v-for="(budget, index) in budgetsWithClasses" class="flex m-1 sidemenu-item" :class=budget.class>
           <div>
             <button @click="visible = true" class="btn-icon btn-icon-success"><i class="pi pi-pencil" /></button>
+            <button @click="deleteBudget(index)" class="btn-icon btn-icon-danger"><i class="pi pi-ban" /></button>
           </div>
-          <div class="ml-2">
+          <div @click="get(index)" class="ml-2">
             <h3>{{ budget.name }}</h3>
             <span>{{ budget.categories_sum_category_limit | 0 }} / {{ budget.limit }} zł</span>
           </div>
@@ -25,6 +25,7 @@
   </Dialog>
 </template> 
 <script setup lang="ts">
+import axios from "axios";
 import { useBudgets } from "@/../utils/useBudgets";
 import { onMounted, ref } from "vue";
 import { budget } from "@/consts/budgetID"
@@ -38,11 +39,14 @@ const visible = ref(false)
 const { getBudgets, budgets } = useBudgets();
 const { getMonth, getYear } = useDate()
 const budgetsWithClasses = ref()
-onMounted(async () => {
-  await getBudgets();
-  addClassesToBudgets()
+onMounted(() => {
+  prepareData();
   //budget.id = budgets.value[0].id
 });
+const prepareData = async () => {
+  await getBudgets();
+  addClassesToBudgets()
+}
 const get = (arrayId: number) => {
   addClassesToBudgets()
   budget.id = budgets.value[arrayId].id
@@ -60,6 +64,12 @@ const closeModal = async () => {
   await getBudgets()
   visible.value = false
 }
+const deleteBudget = (arrayId: any) => {
+  const budgetId = budgetsWithClasses.value[arrayId].id
+  axios.delete(`/api/budgets/${budgetId}`).then(() => {
+    prepareData()
+  });
+}
 const changeDate = async (date: any) => {
   await getBudgets(getMonth(date), getYear(date));
   budget.id = budgets.value[0].id
@@ -72,18 +82,22 @@ const changeDate = async (date: any) => {
   color: black;
   cursor: pointer;
 }
+
 .active {
   background-color: rgba(207, 207, 207) !important;
   color: black !important;
 }
+
 .month-picker {
   position: absolute;
   right: 20%;
 }
+
 .datatable {
   background-color: white;
   border-radius: 10px;
 }
+
 .sidemenu-item {
   align-items: center;
   background-color: white;
